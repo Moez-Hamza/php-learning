329736c43481f99e9fd2a0bd9384817476e1a5d1
Moez Hamza <hamza.moez@gmail.com>
2022-11-28 14:14:55 +0100

    start learning

---
+++ 01_output.php
@@ -0,0 +1,22 @@
+<?php
+    // echo [1,2,3];
+    // echo "hello", 123 , 10.5;
+    // print_r([1,2,3]);
+    // var_dump(true);
+    // var_export('hello');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <h1><?php echo'Post One' ?></h1>
+</body>
+</html>
